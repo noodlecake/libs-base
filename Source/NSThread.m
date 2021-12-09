c502d1a01e0e295ca17b84dab37472946ea6aa5a
Spencer Clark <spencer.d.clark@gmail.com>
2021-12-09 15:31:20 -0600

    Revert "Revert "commented out pthread spin lock dummies""
    This reverts commit 2ece1c138a082bcf2ccacf44f1be0c89cc1d63b0.

--- Source/NSThread.m
+++ Source/NSThread.m
@@ -37,27 +37,27 @@
 
 // Dummy implementatation
 // cleaner than IFDEF'ing the code everywhere
-#if !(HAVE_PTHREAD_SPIN_LOCK)
-#warning no spin_locks, using dummy versions
-typedef int pthread_spinlock_t;
-int pthread_spin_init(pthread_spinlock_t *lock, int pshared)
-{
-  NSDebugLog(@"NSThread.m: Warning this platform does not support spin locks");
-  return 0;
-}
-int pthread_spin_lock(pthread_spinlock_t *lock)
-{
-  return 0;
-}
-int pthread_spin_unlock(pthread_spinlock_t *lock)
-{
-  return 0;
-}
-int pthread_spin_destroy(pthread_spinlock_t *lock)
-{
-  return 0;
-}
-#endif
+// #if !(HAVE_PTHREAD_SPIN_LOCK)
+// #warning no spin_locks, using dummy versions
+// typedef int pthread_spinlock_t;
+// int pthread_spin_init(pthread_spinlock_t *lock, int pshared)
+// {
+//   NSDebugLog(@"NSThread.m: Warning this platform does not support spin locks");
+//   return 0;
+// }
+// int pthread_spin_lock(pthread_spinlock_t *lock)
+// {
+//   return 0;
+// }
+// int pthread_spin_unlock(pthread_spinlock_t *lock)
+// {
+//   return 0;
+// }
+// int pthread_spin_destroy(pthread_spinlock_t *lock)
+// {
+//   return 0;
+// }
+//#endif
 
 /** Structure for holding lock information for a thread.
  */
